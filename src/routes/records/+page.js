cf9af23f1d7c989b49b11239ec735569376ca053
salmorisi-max <salmorisi@gmail.com>
2025-11-01 23:33:15 -0400

    Update +page.js

--- src/routes/records/+page.js
+++ src/routes/records/+page.js
@@ -1,16 +1,3 @@
-import { getLeagueRecords, getLeagueTeamManagers, getLeagueTransactions, waitForAll } from '$lib/utils/helper';
-
-export async function load() {
-    const recordsInfo = waitForAll(
-        getLeagueRecords(false),
-        getLeagueTransactions(false),
-        getLeagueTeamManagers(),
-    )
-
-    return {
-        recordsInfo
-    };
-}
 import {
   getLeagueRecords,
   getLeagueTeamManagers,
